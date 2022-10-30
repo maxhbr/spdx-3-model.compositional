a657bfbb2c1ea6b6eb33c412a490ac6dc3daca54
Maximilian Huber <gh@maxhbr.de>
2022-10-30 20:56:29 +0100

    WIP
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,56 @@
+#![feature(core_intrinsics)]
 use iri_string::types::IriStr;
 use serde_derive::{Deserialize, Serialize};
+use std::intrinsics::type_name;
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
 
-#[derive(Debug, Deserialize, Serialize)] 
+// #[derive(Debug, Deserialize, Serialize)] 
 struct CreationInformation {
 
 }
 
-trait Element {
-    // fn spdxid(&self) -> IriStr;
-    fn name(&self) -> String;
+trait HasSpdxid {
+    fn spdxid(&self) -> Box<IriStr>;
 }
 
-#[derive(Debug, Deserialize, Serialize)] 
+trait Identifyable {
+    fn spdxid(&self) -> Box<IriStr>;
+}
+
+trait Elementic {
+    fn name(&self) -> String;
+    fn summary(&self) -> String;
+    fn description(&self) -> String;
+    fn comment(&self) -> String;
+}
+
+// #[derive(Debug, Deserialize, Serialize)] 
+#[derive(Debug)]
 struct ElementImpl {
-    spdxid: Box<IriStr>,
     name: String,
     summary: String,
     description: String,
     comment: String,
 }
 
-// impl Element for ElementImpl {
-//     // fn spdxid(&self) -> IriStr {
-//     //     *self.spdxid()
-//     // }
-//     fn name(&self) -> String {
-//         *self.name
-//     }
-// }
+impl Identifyable for ElementImpl {
+    fn spdxid(&self) -> Box<IriStr> {
+        let structname =  unsafe { type_name::<Self>() };
+        let mut hasher = DefaultHasher::new();
+        hasher.write(format!("{:?}", self).as_bytes());
+        let hash = hasher.finish();
+        let iri_str_raw = format!("urn:{}:{}", structname, hash);
+        IriStr::new(iri_str_raw.as_str()).unwrap().into()
+    }
+}
 
+impl Elementic for ElementImpl {
+    fn name(&self) -> String { self.name.clone() }
+    fn summary(&self) -> String { self.summary.clone() }
+    fn description(&self) -> String { self.description.clone() }
+    fn comment(&self) -> String { self.comment.clone() }
+}
 
 #[cfg(test)]
 mod tests {
@@ -37,7 +59,6 @@ mod tests {
     #[test]
     fn it_works() {
         let e = ElementImpl {
-            spdxid: IriStr::new("urn:spdx.dev").unwrap().into(),
             name: String::from("Element1"),
             summary: String::from("Summary1"),
             description: String::from("Description1"),
